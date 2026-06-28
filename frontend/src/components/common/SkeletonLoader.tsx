252fdd3ede0aead3357eae159f84fd9329f76535
kirtisharma-sudo <sharma.kirti.7002@gmail.com>
2026-06-28 18:19:30 +0530

    Add SkeletonLoader component for loading states

---
+++ frontend/src/components/common/SkeletonLoader.tsx
@@ -0,0 +1,7 @@
+export const SkeletonLoader = () => (
+  <div className="animate-pulse space-y-2">
+    <div className="h-4 bg-space-border/30 rounded w-3/4"></div>
+    <div className="h-4 bg-space-border/30 rounded w-1/2"></div>
+    <div className="h-64 bg-space-border/20 rounded"></div>
+  </div>
+);
